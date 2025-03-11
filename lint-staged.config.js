c5ea0e0177af5c1487cee8f7dea1825a592f7951
affandhia <affandhia@gmail.com>
2025-03-11 20:01:09 +0800

    fix: update lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   // Run ESLint on JS, JSX, TS, and TSX files
-  '**/*.{js,jsx,ts,tsx}':
-   ['eslint --fix', 'prettier --write'],
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
 
   // Run Prettier on other file types
-  '**/*.{css,scss,json,md,mdx,html}': 
-  ['prettier --write'],
+  '**/*.{css,scss,json,md,mdx,html}': ['prettier --write'],
 
   // Specific handling for package.json
-  'package.json': [
-    'prettier --write',
-  ],
+  'package.json': ['prettier --write'],
 };
